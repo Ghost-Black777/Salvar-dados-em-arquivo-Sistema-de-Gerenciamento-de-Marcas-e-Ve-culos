dd3d04acb8b3df28e82212f25944203c99118171
Hugo Felipe <felipehugo137@gmail.com>
2025-10-04 22:51:30 -0300

    Add files via upload

---
+++ PrjMarcaVeiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjmarcaveiculo;
+
+import com.mycompany.prjmarcaveiculo.menu.MenuPrincipal;
+
+
+public class PrjMarcaVeiculo {
+
+    public static void main(String[] args) {
+        
+        MenuPrincipal menuP = new MenuPrincipal();
+        menuP.menuPrincipal();
+        
+    }
+}
